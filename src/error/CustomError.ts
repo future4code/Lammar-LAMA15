c37f5469a1a7041c27c6f317999cd46d6760ce53
Henrique Douglas Araujo <araujohdouglas@gmail.com>
2023-03-14 21:03:32 -0300

    create project base

---
+++ src/error/CustomError.ts
@@ -0,0 +1,5 @@
+export abstract class CustomError extends Error {
+    constructor(public statusCode:number, public message:string) {
+        super(message)
+    }
+}
---
+++ src/services/IdGenerator.ts
@@ -0,0 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export class IdGenerator{
+
+    public generate(): string{
+        return uuidv4()
+    }
+}
